10578d471806d820bf44d3d71d3e8addcfaf4224
Abhay Joshi <jabhay2012@gmail.com>
2025-02-18 14:56:44 -0500

    removed useref

--- app/page.tsx
+++ app/page.tsx
@@ -124,7 +124,7 @@
 
 
 'use client'
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 // Define types for the response data
