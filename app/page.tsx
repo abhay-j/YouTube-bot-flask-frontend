95d7754e4eedfaad971e536ce70d5ee522fc18b1
Abhay Joshi <jabhay2012@gmail.com>
2025-03-04 20:18:02 -0800

    ESLint error fix

--- app/page.tsx
+++ app/page.tsx
@@ -453,7 +453,9 @@ export default function Home() {
       
       {/* Show initial prompt below the search bar */}
       {!searchQuery && !response && (
-        <p className="text-gray-500 text-center">Search through all videos from Overpowered.AI to get relevant information (Currently under developement, please use these queries to test : "Tell me about Midjourney", "Text to image tools", "Tell me about Suno AI") </p>
+        <p className="text-gray-500 text-center">  Search through all videos from Overpowered.AI to get relevant information (Currently under development, please use these queries to test: Tell me about Midjourney , Text to image tools, Tell me about Suno AI)
+ </p>
+
       )}
       
       {/* Loading State */}
